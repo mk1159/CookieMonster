f2f70bdff4ed646fa96bedf1f5a635d665ff7564
mk1159 <20940977+mk1159@users.noreply.github.com>
2019-03-03 10:08:37 -0800

    removed unnecessary directory
     #changed unnecessary directory

--- video_scripts/downloadYTPlaylistIronMan.py
+++ video_scripts/downloadYTPlaylistIronMan.py
@@ -2,4 +2,4 @@ from pytube import Playlist
 pl = Playlist("https://www.youtube.com/playlist?list=PLDs6Txl1GILzoZmUQpJkZf9o8ZkaMFKcB")
 #pl.download_all()
 # or if you want to download in a specific directory
-pl.download_all('/Users/hamzakhan/Documents/GitHub/cookieMonster/youtubeIronmanMP4')
+pl.download_all('./cookieMonster/youtubeIronmanMP4')  #changed unnecessary directory
