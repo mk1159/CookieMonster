62cae7aef307673e5913c516128cffe8c030d315
mk1159 <20940977+mk1159@users.noreply.github.com>
2019-03-03 10:09:05 -0800

    Removed unnecessary directory
     #changed unnecessary directory

--- video_scripts/downloadYTPlaylistAllElse.py
+++ video_scripts/downloadYTPlaylistAllElse.py
@@ -2,4 +2,4 @@ from pytube import Playlist
 pl = Playlist("https://www.youtube.com/playlist?list=PLzwP33RAZTVA7-IxNZWCEr4rDZn2_PQ_F")
 #pl.download_all()
 # or if you want to download in a specific directory
-pl.download_all('/Users/hamzakhan/Documents/GitHub/cookieMonster/allElse')
+pl.download_all('./cookieMonster/allElse') #changed unnecessary directory
